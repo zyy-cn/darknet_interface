3896784ed9888b1683d5db39cd27612d947dcdeb
YinZhaoYuan <zacchues@qq.com>
2018-06-13 11:19:57 +0800

    remove 'example' and 'lib_library'

--- lib_detector/gcc.sh
+++
@@ -1,1 +0,0 @@
-g++ -fPIC -shared -O3 -o libdetector.so detector.cpp -I. -I../../darknet_Alexey/src -I/home/pi/local_install/include -L/home/pi/Code/darknet_Alexey/ -ldarknet -fopenmp -lgomp -DOPENCV -DOPENBLAS
